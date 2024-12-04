6c57d12d25acc4cf371a7a11d235f0980f29430a
Daniel DiLoreto <diloretodg74122@gmail.com>
2024-12-03 22:33:13 -0500

    Add BasicSynth example with timer-based audio generation
    Add BasicSynth example with timer-based audio generation

--- include/Constants.h
+++ include/Constants.h
@@ -1,9 +1,10 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-// Timer Configuration
-#define TIMER_PRESCALER 8
-#define TIMER_COMPARE_VALUE 249 // For 16MHz clock, 8 prescaler, 8kHz sampling
-#define TIMER_INTERRUPT_FREQ 8000
+#define TIMER_GROUP 0
+#define TIMER_DIVIDER 80
+#define TIMER_INTERVAL 50 // microseconds
+
+#define DAC_CHANNEL_1 DAC_CHANNEL_1
 
 #endif
