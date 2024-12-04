8731ecdfe0091ba67151b790c5b412b2e88a4a35
Daniel DiLoreto <diloretodg74122@gmail.com>
2024-12-03 22:14:56 -0500

    Improve timer setup and use std::array for better type safety
    Improve timer setup and use std::array for better type safety

--- src/core/AudioEffects.h
+++ src/core/AudioEffects.h
@@ -1,1 +1,8 @@
 // src/core/AudioEffects.h
+
+class Effects {
+private:
+    static constexpr size_t MAX_DELAY_SAMPLES = 4096;
+    std::array<float, MAX_DELAY_SAMPLES> delayBuffer;
+    size_t delayIndex = 0;
+    float feedback = 0.3f;
--- src/core/GenerativeSequencer.h
+++ src/core/GenerativeSequencer.h
@@ -1,1 +1,12 @@
 // src/core/GenerativeSequencer.h
+class GenerativeSequencer {
+private:
+    static constexpr size_t MAX_STEPS = 16;
+    SequenceStep steps[MAX_STEPS];
+    std::array<float, AUDIO_BUFFER_SIZE> audioBuffer;
+    uint8_t sequenceLength;
+    uint8_t currentStep;
+    float phase;
+    float sampleRate;
+    EvolutionParameters evoParams;
+    EvolutionVisualizer& visualizer;
